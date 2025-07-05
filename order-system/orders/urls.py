692edaec55fb841e6fe828b5a199c843beed07e7
dimmav <dimitrisv2010@gmail.com>
2025-07-05 11:21:12 +0300

    Dashboard

--- order-system/orders/urls.py
+++ order-system/orders/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import CustomerViewSet, ItemViewSet, OrderViewSet, OrderItemViewSet, PaymentViewSet
+
+from .views import (CustomerViewSet, DashboardViewSet, ItemViewSet,
+                    OrderItemViewSet, OrderViewSet, PaymentViewSet)
 
 router = DefaultRouter()
 router.register(r'customers', CustomerViewSet)
@@ -8,6 +10,7 @@ router.register(r'items', ItemViewSet)
 router.register(r'orders', OrderViewSet)
 router.register(r'order-items', OrderItemViewSet)
 router.register(r'payments', PaymentViewSet)
+router.register(r'dashboard', DashboardViewSet, basename='dashboard')  # 🔹 ΠΡΟΣΟΧΗ σε αυτό!
 
 urlpatterns = [
     path('', include(router.urls)),
