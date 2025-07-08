ae7dc0fac33832786748e85a7877e241c6c5be55
dimmav <dimitrisv2010@gmail.com>
2025-07-08 13:26:03 +0300

    eidi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import ProtectedRoute from './components/ProtectedRoute';
+import CustomerDetail from './pages/CustomerDetail';
+import CustomerForm from './pages/CustomerForm';
 import Customers from './pages/Customers';
 import Dashboard from './pages/Dashboard';
 import Items from './pages/Items';
 import Login from './pages/Login';
 import Orders from './pages/Orders';
 import Payments from './pages/Payments';
-
 function App() {
   return (
     <Router>
@@ -25,6 +26,10 @@ function App() {
                   <Route path="/items" element={<Items />} />
                   <Route path="/orders" element={<Orders />} />
                   <Route path="/payments" element={<Payments />} />
+                  <Route path="/customers/new" element={<CustomerForm />} />
+                  <Route path="/customers/:id/edit" element={<CustomerForm />} />
+                  <Route path="/customers/:id" element={<CustomerDetail />} />
+                  <Route path="/items" element={<Items />} />
                 </Routes>
               </Layout>
             </ProtectedRoute>
